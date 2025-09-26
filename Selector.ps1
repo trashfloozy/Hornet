ff9870af155da2cfc3102ac5cba6178d85f58cf3
trashfloozy <gabubble2002@live.com>
2025-09-26 02:38:11 -0400

    Add files via upload

---
+++ Selector.ps1
@@ -0,0 +1,25 @@
+if ($PSVersionTable.PSVersion.Major -lt 7) {
+    Write-Host "Restarting in PowerShell 7..."
+    $pwsh = "pwsh.exe"   # path to PowerShell 7
+    & $pwsh -File $PSCommandPath @args
+    exit
+}
+
+Clear-Host
+Install-Module Microsoft.PowerShell.ConsoleGuiTools
+$choices = @("Hornet", "Exit")
+
+$userInput = $choices | Out-ConsoleGridView -Title "Select your game and press enter!" -OutputMode Single
+
+switch ($userInput) {
+    "Hornet" {
+        Invoke-WebRequest -useb https://raw.githubusercontent.com/trashfloozy/Hornet/refs/heads/main/Scripts/Silksong.ps1 | Invoke-Expression
+    }
+    "Exit" {
+        Write-Host -Object "Exiting..."
+        exit
+    }
+    default {
+        Write-Host -Object "Unknown answer." -ForegroundColor Red
+    }
+}

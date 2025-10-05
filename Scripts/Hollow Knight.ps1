aa3fb0612aeacc42d4dc8c7ead9863acbbbd091f
trashfloozy <gabubble2002@live.com>
2025-10-04 22:33:22 -0400

    Add files via upload

---
+++ Scripts/Hollow Knight.ps1
@@ -0,0 +1,178 @@
+$hkSavePath = "$env:USERPROFILE\AppData\LocalLow\Team Cherry\Hollow Knight\"
+$hkBackupPath = "$env:USERPROFILE\Desktop\Steam Backup Save\"
+
+$emergencySave = "$env:USERPROFILE\Desktop\Steam Backup Save\EMERGENCY\Hollow Knight\"
+
+$hkBackupedPath = "$env:USERPROFILE\Desktop\Steam Backup Save\Hollow Knight\"
+$hkRestorePath = "$env:USERPROFILE\AppData\LocalLow\Team Cherry\"
+
+function CopyBackup {
+  [CmdletBinding()]
+  param ()
+  process {
+    if (!(Test-Path -Path $hkBackupPath)) {
+      New-Item -ItemType Directory -Path $hkBackupPath > $null
+    }
+    
+    Copy-Item -Path "$hkSavePath" -Destination $hkBackupPath -Recurse -Force
+    Write-Host -Object '> Saved' -ForegroundColor 'Green'
+  }
+}
+
+function emergencyBackup {
+  [CmdletBinding()]
+  param()
+  process {
+    if (!(Test-Path -Path $hkSavePath)) {
+      Write-Host -Object 'GAME DIRECTORY NOT DETECTED. ABORTED.' -ForegroundColor 'Red'
+      Pause
+      exit
+    }
+
+
+    if (!(Test-Path -Path $emergencySave)) {
+      Write-Host -Object 'No emergency backup found. Create one now?' -ForegroundColor 'Red'
+      $readUser = [System.Console]::ReadKey($true)
+      $translateUser = $readUser.KeyChar
+      $emergencyInput = $translateUser.ToString().ToLower()
+      
+      switch ($emergencyInput.ToLower()) {
+        {($_ -eq "y") -or ($_ -eq "yes")} {
+          Copy-Item -Path "$hkSavePath" -Destination $emergencySave -Recurse -Force
+          Write-Host -Object '> Saved Emergency Backup' -ForegroundColor 'Green'
+          exit
+        }
+        {($_ -eq "n") -or ($_ -eq "no")} {
+          exit
+        }
+        default {
+          Write-Host -Object 'Unknown input.' -ForegroundColor 'Red'
+          Pause
+          exit
+        }
+      }
+    }
+    else {
+      Write-Host -Object 'Update emergency backup? This CANNOT be undone! [Y/N]' -ForegroundColor 'Red'
+      $readUser = [System.Console]::ReadKey($true)
+      $translateUser = $readUser.KeyChar
+      $emergencyInput = $translateUser.ToString().ToLower()
+
+      switch ($emergencyInput.ToLower()) {
+        {($_ -eq "y") -or ($_ -eq "yes")} {
+          Copy-Item -Path "$hkSavePath" -Destination $emergencySave -Recurse -Force
+          Write-Host -Object '> Saved Emergency Backup' -ForegroundColor 'Green'
+          exit
+        }
+        {($_ -eq "n") -or ($_ -eq "no")} {
+          exit
+        }
+        default {
+          Write-Host -Object 'Unknown input.' -ForegroundColor 'Red'
+          Pause
+          exit
+        }
+      }
+    }
+  }
+}
+
+function RestoreBackup {
+  [CmdletBinding()]
+  param ()
+  process {
+    if (!(Test-Path -Path $hkBackupedPath)) {
+      Write-Host -Object 'No backup directory found. Create a backup before proceeding!' -ForegroundColor 'Red'
+      Pause
+      exit
+    }
+
+    if (Test-Path -Path $hkSavePath) {
+      Remove-Item -Path $hkSavePath -Recurse -Force
+    }
+      
+    Copy-Item -Path "$hkBackupedPath" -Destination $hkRestorePath -Recurse -Force
+    Write-Host -Object '> Restored' -ForegroundColor 'Green'
+  }
+}
+
+function backupSecurity {
+  if (!(Test-Path -Path $hkSavePath)) {
+    Write-Host -Object 'GAME DIRECTORY NOT DETECTED. SOMETHING IS WRONG. VERIFY GAME IS INSTALLED AND HAS AT LEAST ONE SAVE FILE.' -ForegroundColor 'Red'
+    Pause
+    exit
+  }
+}
+
+function restoreSecurity {
+  if (!(Test-Path -Path $hkSavePath)) {
+    Write-Host -Object 'GAME DIRECTORY NOT DETECTED. PROCEED WITH RESTORATION ANYWAY? [Y/N]' -ForegroundColor 'Red'
+    $readUser = [System.Console]::ReadKey($true)
+    $translateUser = $readUser.KeyChar
+    $userInput = $translateUser.ToString().ToLower()
+
+    switch ($userInput.ToLower()) {
+      {($_ -eq "y") -or ($_ -eq "yes")} {
+        Write-Host -Object 'Continuing...' -ForegroundColor 'Yellow'
+        break
+      }
+      {($_ -eq "n") -or ($_ -eq "no")} {
+        Write-Host -Object 'ABORTED!' -ForegroundColor 'Red'
+        Pause
+        exit
+      }
+      default {
+        Write-Host -Object 'Unknown input.' -ForegroundColor 'Red'
+        exit
+      }
+    }
+  }
+}
+
+function emergencySecurity {
+  if (!(Test-Path -Path $emergencySave)) {
+    Write-Host -Object 'NO EMERGENCY SAVE DETECTED. CHECKING GAME DIRECTORY...' -ForegroundColor 'Red'
+    
+    if (!(Test-Path -Path $hkSavePath)) {
+      Write-Host -Object 'GAME SAVE NOT FOUND. BACKUP IS UNAVAILABLE.' -ForegroundColor 'Red'
+      Pause
+    } else {
+      Copy-Item -Path "$hkSavePath" -Destination "$emergencySave\Hollow Knight" -Recurse -Force
+      Write-Host -Object '> Saved Emergency Backup' -ForegroundColor 'Green'
+    }
+  }
+}
+
+
+$Host.UI.RawUI.Flushinputbuffer()
+
+Clear-Host
+emergencySecurity
+
+Write-Host -Object 'Do you want to backup or restore your saves? [B/R/E]' -ForegroundColor 'Yellow'
+$readUser = [System.Console]::ReadKey($true)
+$translateUser = $readUser.KeyChar
+$backupInput = $translateUser.ToString().ToLower()
+
+switch ($backupInput.ToLower()) {
+  {($_ -eq "b") -or ($_ -eq "backup")} {
+    backupSecurity
+    CopyBackup
+    exit
+  }
+  {($_ -eq "r") -or ($_ -eq "restore")} {
+    restoreSecurity
+    RestoreBackup
+    exit
+  }
+  {($_ -eq "e") -or ($_ -eq "emergency")} {
+    #emergencySecurity
+    emergencyBackup
+    exit
+  }
+  default {
+    Write-Host -Object 'Unknown input.' -ForegroundColor 'Red'
+    Pause
+    exit
+  }
+}

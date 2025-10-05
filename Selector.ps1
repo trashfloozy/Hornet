6bc581ec95d56e6de2aad0602cc58b5075e889db
trashfloozy <gabubble2002@live.com>
2025-10-04 22:44:04 -0400

    Update Selector.ps1

--- Selector.ps1
+++ Selector.ps1
@@ -2,6 +2,7 @@ if ($PSVersionTable.PSVersion.Major -lt 7) {
     Write-Host "Restarting in PowerShell 7..."
     $pwsh = "pwsh.exe"   # path to PowerShell 7
     & $pwsh -File $PSCommandPath @args
+    Pause
     exit
 }
 
@@ -14,22 +15,19 @@ $userInput = $choices | Out-ConsoleGridView -Title "Select your game and press e
 switch ($userInput) {
     "Silksong" {
         Invoke-WebRequest -useb https://raw.githubusercontent.com/trashfloozy/Hornet/refs/heads/main/Scripts/Silksong.ps1 | Invoke-Expression
-        Pause
     }
     "Hollow Knight" {
         Invoke-WebRequest -useb https://raw.githubusercontent.com/trashfloozy/Hornet/refs/heads/main/Scripts/Hollow%20Knight.ps1 | Invoke-Expression
-        Pause
     }
     "Exit" {
         Write-Host -Object "Exiting..."
         Pause
-        exit
     }
     default {
         Write-Host -Object "Unknown answer." -ForegroundColor Red
-        Pause
     }
     Pause
 }
+
 
 
